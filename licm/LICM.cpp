86210e6d5494fe4bff00d65e84265155bd9436e9
Enochen <sirenochen@gmail.com>
2023-10-14 21:37:57 -0400

    fix pass registration and add preheader

--- licm/LICM.cpp
+++ licm/LICM.cpp
@@ -1,6 +1,9 @@
+#include "llvm/Analysis/ValueTracking.h"
 #include "llvm/Pass.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include <iostream>
 
 using namespace llvm;
 
@@ -43,15 +46,19 @@ struct LICMPass : public PassInfoMixin<LICMPass> {
     auto preheader = BasicBlock::Create(header->getContext(),
                                         header->getName() = "_preheader",
                                         header->getParent(), header);
-    BranchInst::Create(header, preheader);
+    auto jmp = BranchInst::Create(header, preheader);
     loop->addBasicBlockToLoop(preheader, LI);
+    loop->moveToHeader(preheader);
 
     // Get all predecessors of header outside loop
+    auto outside_preds = std::vector<BasicBlock *>();
     for (auto *pred : predecessors(header)) {
       if (!loop->contains(pred)) {
+        outside_preds.push_back(pred);
         pred->getTerminator()->replaceSuccessorWith(header, preheader);
       }
     }
+    // probably need to do something with outside_preds to fix phi nodes
   }
 };
 
@@ -60,9 +67,18 @@ struct LICMPass : public PassInfoMixin<LICMPass> {
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
   return {.APIVersion = LLVM_PLUGIN_API_VERSION,
-          .PluginName = "LICM pass",
+          .PluginName = "LICMPass",
           .PluginVersion = "v0.1",
           .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef name, FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (name == "LICMPass") {
+                    FPM.addPass(LICMPass());
+                    return true;
+                  }
+                  return false;
+                });
             PB.registerPipelineStartEPCallback(
                 [](ModulePassManager &MPM, OptimizationLevel Level) {
                   MPM.addPass(createModuleToFunctionPassAdaptor(LICMPass()));
