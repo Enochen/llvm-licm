4e273b750cd97c31ce9d90ff46b7273a3bfb0e06
Zak Kent <73757337+zachary-kent@users.noreply.github.com>
2023-10-16 16:44:02 -0400

    Update test.sh
    Co-authored-by: Enoch Chen <sirenochen@gmail.com>

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 clang -Xclang -disable-O0-optnone -O0 -emit-llvm -S test/loop_sum.c -o loop_sum.ll
 
 # opt is used to apply specific optimizations
+OPT_PATH=opt
 if [[ "$OSTYPE" == "darwin"* ]]; then 
-  `brew --prefix llvm`/bin/opt -S -passes=mem2reg loop_sum.ll -o output_mem2reg.ll
-  `brew --prefix llvm`/bin/opt -S --load-pass-plugin=`ls build/licm/LICMPass.*` -passes=LICMPass output_mem2reg.ll -o output_licm.ll
-else 
-  opt -S -load-pass-plugin build/licm/LICMPass.so -passes=mem2reg loop_sum.ll -o output_mem2reg.ll
-  opt -S -load-pass-plugin build/licm/LICMPass.so -passes=LICMPass output_mem2reg.ll -o output_licm.ll
+  OPT_PATH=`brew --prefix llvm`/bin/opt
 fi
+
+$OPT_PATH -S -passes=mem2reg loop_sum.ll -o output_mem2reg.ll
+$OPT_PATH -S -load-pass-plugin=`ls build/licm/LICMPass.*` -passes=LICMPass output_mem2reg.ll -o output_licm.ll
